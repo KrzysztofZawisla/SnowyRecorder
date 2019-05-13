f59e4aad8f154ae8c2ad93ecd5146e988d4a8330
Krzysztof Zawisła <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-05-13 22:50:12 +0200

    Create dragbar.ts

---
+++ src-ts/JS/dragbar.ts
@@ -0,0 +1,17 @@
+const dragbar:any = document.getElementById("dragbar");
+
+function dragbarChange():void {
+   dragbar.innerHTML = "Przytrzyjmaj tą belkę aby przesunąć program";
+   setTimeout(() => {
+      dragbar.innerHTML = "Ctrl+Q - wyjście";
+   }, 5000);
+   setTimeout(() => {
+      dragbar.innerHTML = "Ctrl+R - przeładowanie";
+   }, 10000);
+   setTimeout(() => {
+      dragbar.innerHTML = "Ctrl+Shift+R - restart";
+   }, 15000);
+   setTimeout(dragbarChange, 20000);
+}
+
+dragbarChange();
