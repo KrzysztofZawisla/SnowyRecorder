a9e3a8a875854ce19ba68d8795b74d6f4231492b
Krzysztof Zawisła <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-05-13 23:14:11 +0200

    Delete modules.ts

--- src-ts/JS/modules.ts
+++
@@ -1,7 +0,0 @@
-const AudioRecorder:any = require('node-audiorecorder');
-const fs:any = require(`fs`);
-const path:any = require(`path`);
-const os:any = require("os");
-const electron:any = require("electron");
-
-const { remote, ipcRenderer }:any = electron;
